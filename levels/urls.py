a891189eeeb3a13b562691b5ec784ea468725e27
Solomon Kebede <solomonaau188@gmail.com>
2023-12-26 22:15:11 +0000

    Add route and view for level 1

--- levels/urls.py
+++ levels/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('level0', views.level0, name='level0'),
+    path('level1', views.level1, name='level1'),
 ]
--- levels/views.py
+++ levels/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def level0(request):
     return render(request, 'levels/level0.html')
+
+def level1(request):
+    return render(request, 'levels/level1.html')
