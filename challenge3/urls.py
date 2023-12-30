0fcd04007bf9c634be615d99a29c73888f448090
Solomon Kebede <solomonaau188@gmail.com>
2023-12-30 13:57:22 +0000

    Add routes and views for venicodivici pages

--- challenge3/urls.py
+++ challenge3/urls.py
@@ -20,6 +20,9 @@ from django.urls import path, include
 from django.shortcuts import render
 from django.shortcuts import redirect
 
+from django.http import HttpResponse, FileResponse
+from django.conf import settings
+
 def custom_404_view_2(request, url):
     return render(request, '404/404.html', status=404)
 
@@ -29,11 +32,30 @@ def projects_view(request):
 def index(request):
     return redirect('projects/100018')
 
+def venicodivici_page1(request):
+    ''' http://www.venicodivici.com/crackme '''
+    # File path relative to the app and template directory
+    import os
+    file_path = os.path.join(settings.BASE_DIR, 'levels', 'templates', 'venicodivici', 'crackme')
+    # Open the file in binary mode
+    with open(file_path, 'rb') as f:
+        response = HttpResponse(f.read(), content_type='application/octet-stream')
+    # Set the Content-Disposition header to force download
+    response['Content-Disposition'] = 'attachment; filename="crackme"'
+
+    return response
+    # return render(request, 'venicodivici/crackme')
+
+def venicodivici_page0(request):
+    return render(request, 'venicodivici/index.html')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='home_page'),
     path('ch4ll3n632022/', include('levels.urls')),
     path('projects/100018', projects_view, name='projects'),
+    path('www.venicodivici.com/crackme', venicodivici_page1, name='www.venicodivici.com/crackme'),
+    path('www.venicodivici.com/', venicodivici_page0, name='www.venicodivici.com/'),
     # The following are to mock the error pages
     path('<str:url>', custom_404_view_2, name='match_url'),
     path('<str:url>/', custom_404_view_2, name='match_url'),
